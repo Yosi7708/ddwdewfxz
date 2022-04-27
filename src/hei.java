0f6a4823fe021b8330d8b09c3d8557207a00c7df
yosi7708 <yosi@064.com>
2022-04-27 12:38:57 +0300

    fix bug of null pointer 2

--- src/hei.java
+++ src/hei.java
@@ -2,6 +2,7 @@ public class hei {
     public static void main (String [] args){
         System.out.println("heloos");
         System.out.println("ssss");
+        System.out.println(4+2);
 
     }
 }
